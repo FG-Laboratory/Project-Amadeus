3bddc7a521b83bae913ef287819a1fad9151563c
nikminer <nikminerfeed@gmail.com>
2021-06-16 22:50:13 +0300

    Модуль отложенного отключения

---
+++ moduls/Shutdown.py
@@ -0,0 +1,32 @@
+import platform
+import subprocess
+
+def Shutdown(minutes=0):
+    if platform.system() == 'Windows':
+        subprocess.call('shutdown /s /t {}'.format(minutes * 60), shell=True)
+        return True
+    elif platform.system() == 'Linux':
+        if minutes == 0:
+            subprocess.call('shutdown -p now', shell=True)
+        else:
+            subprocess.call('shutdown -p +{}'.format(minutes), shell=True)
+        return True
+    else:
+        return False
+
+
+def AbortShutdown():
+    if platform.system() == 'Windows':
+        subprocess.call('shutdown /a', shell=True)
+        return True
+    elif platform.system() == 'Linux':
+        subprocess.call('shutdown -a', shell=True)
+        return True
+    else:
+        return False
+
+
+# Для тестирования при прямом запуске этот скрипт должен запланировать а затем отменить выключение винды
+if __name__ == '__main__':
+    Shutdown(minutes=100)
+    AbortShutdown()
